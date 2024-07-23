f279d2047f66ee1c5b581c75e38831c101382d9b
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-07-23 15:31:47 +0200

    feat: add methods to measure string length and replace badwords in the text message.

--- pagina-di-atterraggio.php
+++ pagina-di-atterraggio.php
@@ -1,6 +1,16 @@
 <?php 
 
+//dichiarare una variabile in PHP da legare al messagio in form
 $message = $_POST['message'];
+
+//calcolare il numero dei caratteri del messaggios scritto dall'utente
+$length_message = strlen($message);
+
+//censurare le parole volgari sostituendole con asterischi
+$correct_message = str_replace('cazzo', '***', $message);
+
+//calcolare di nuovo la lunghezza del messaggio
+$length_correct_message = strlen($correct_message)
 
 ?>
 
@@ -15,5 +25,11 @@ $message = $_POST['message'];
     
     <h2>Ecco cosa hai scritto:</h2>
     <p><?php echo $message ?> </p>
+
+    <strong>Numero caratteri: <?php echo $length_message ?></strong>
+
+    <p><?php echo $correct_message ?> </p>
+    <strong>Numero caratteri: <?php echo $length_correct_message ?></strong>
+
 </body>
 </html>
