450674276815e0f496ae17e8939afdd0f138d4fa
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-07-23 14:52:47 +0200

    feat: add php method "$_post" linked to the form and stamp message in the webpage

--- pagina-di-atterraggio.php
+++ pagina-di-atterraggio.php
@@ -1,1 +1,19 @@
-<?php ?>
+<?php 
+
+$message = $_POST['message'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Risposta</title>
+</head>
+<body>
+    
+    <h2>Ecco cosa hai scritto:</h2>
+    <p><?php echo $message ?> </p>
+</body>
+</html>
